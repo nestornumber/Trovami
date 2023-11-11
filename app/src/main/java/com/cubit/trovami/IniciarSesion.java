2a14270579a716d63cb0f33a596f175bde30fb0a
WarReyes <wilfredo.rosales454@gmail.com>
2023-11-11 09:00:20 -0600

    login

--- app/src/main/java/com/cubit/trovami/IniciarSesion.java
+++ app/src/main/java/com/cubit/trovami/IniciarSesion.java
@@ -1,8 +1,11 @@
 package com.cubit.trovami;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class IniciarSesion extends AppCompatActivity {
 
@@ -10,5 +13,20 @@ public class IniciarSesion extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_iniciar_sesion);
+
+        // Obtener referencia al botón "CREAR CUENTA"
+        Button btnCrearCuenta = findViewById(R.id.button4);
+
+        // Configurar el evento de clic para el botón "CREAR CUENTA"
+        btnCrearCuenta.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // Iniciar la actividad Registrarse1
+                Intent intent = new Intent(IniciarSesion.this, Registrarse1.class);
+                startActivity(intent);
+            }
+        });
+
+        // Puedes agregar el resto del código necesario para el inicio de sesión aquí...
     }
 }
--- app/src/main/java/com/cubit/trovami/Registrarse2.java
+++ app/src/main/java/com/cubit/trovami/Registrarse2.java
@@ -1,14 +1,68 @@
 package com.cubit.trovami;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
-
 public class Registrarse2 extends AppCompatActivity {
+
+    private static final String PREFS_NAME = "UserData";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registrarse2);
+
+        // Obtener referencias a las vistas
+        EditText editTextUsuario = findViewById(R.id.editTextText3);
+        EditText editTextContrasena = findViewById(R.id.editTextText4);
+        Button btnCrearCuenta = findViewById(R.id.button4);
+        Button btnRegresar = findViewById(R.id.button2);
+
+        // Configurar el evento de clic para el botón "CREAR CUENTA"
+        btnCrearCuenta.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // Obtener los datos ingresados
+                String usuario = editTextUsuario.getText().toString().trim();
+                String contrasena = editTextContrasena.getText().toString().trim();
+
+                // Verificar si se ingresaron ambos datos
+                if (!usuario.isEmpty() && !contrasena.isEmpty()) {
+                    // Guardar los datos en SharedPreferences
+                    guardarDatosEnSharedPreferences(usuario, contrasena);
+
+                    // Iniciar la actividad IniciarSesion
+                    Intent intent = new Intent(Registrarse2.this, IniciarSesion.class);
+                    startActivity(intent);
+                } else {
+                    // Mostrar mensaje de alerta si no se ingresaron ambos datos
+                    Toast.makeText(Registrarse2.this, "Ingrese usuario y contraseña", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+        // Configurar el evento de clic para el botón "REGRESAR"
+        btnRegresar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // Iniciar la actividad Registrarse1
+                Intent intent = new Intent(Registrarse2.this, Registrarse1.class);
+                startActivity(intent);
+            }
+        });
+    }
+
+    private void guardarDatosEnSharedPreferences(String usuario, String contrasena) {
+        SharedPreferences.Editor editor = getSharedPreferences(PREFS_NAME, MODE_PRIVATE).edit();
+        editor.putString("usuario", usuario);
+        editor.putString("contrasena", contrasena);
+        editor.apply();
     }
 }
