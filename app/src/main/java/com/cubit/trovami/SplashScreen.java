7ad2c9e2452fa40444174ad42893c600cac97e4c
WarReyes <wilfredo.rosales454@gmail.com>
2023-11-11 08:33:42 -0600

    inicio

--- app/src/main/java/com/cubit/trovami/SplashScreen.java
+++ app/src/main/java/com/cubit/trovami/SplashScreen.java
@@ -1,10 +1,9 @@
 package com.cubit.trovami;
-
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class SplashScreen extends AppCompatActivity {
 
@@ -12,15 +11,15 @@ public class SplashScreen extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.splash_screen);
-        Handler manejador=new Handler();
 
-        manejador.postDelayed(new Runnable() {
+        // Esperar 4 segundos y luego iniciar la actividad Presentacion0
+        new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
-                finish();
-                Intent objVentana=new Intent(SplashScreen.this, Busqueda.class);
-                startActivity(objVentana);
+                Intent intent = new Intent(SplashScreen.this, Presentacion0.class);
+                startActivity(intent);
+                finish(); // Cierra la actividad actual para que no pueda regresar a la pantalla de bienvenida.
             }
-        }, 1500);
+        }, 4000); // 4000 milisegundos = 4 segundos
     }
 }
