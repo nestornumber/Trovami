f7923b56a7fb5fd55a1e7853375d65e355462bb2
WarReyes <wilfredo.rosales454@gmail.com>
2023-11-11 13:40:53 -0600

    listo

--- app/src/main/java/com/cubit/trovami/IniciarSesion.java
+++ app/src/main/java/com/cubit/trovami/IniciarSesion.java
@@ -10,9 +10,6 @@ import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import java.util.HashSet;
-import java.util.Set;
-
 public class IniciarSesion extends AppCompatActivity {
 
     private static final String PREFS_NAME = "UserData";
@@ -22,33 +19,43 @@ public class IniciarSesion extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_iniciar_sesion);
 
+        // Obtener referencias a las vistas
         EditText editTextUsuario = findViewById(R.id.editTextText3);
         EditText editTextContrasena = findViewById(R.id.editTextText4);
         Button btnIniciarSesion = findViewById(R.id.button2);
         Button btnCrearCuenta = findViewById(R.id.button4);
 
+        // Configurar el evento de clic para el botón "INICIAR SESIÓN"
         btnIniciarSesion.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                // Obtener los datos ingresados
                 String usuario = editTextUsuario.getText().toString().trim();
                 String contrasena = editTextContrasena.getText().toString().trim();
 
+                // Verificar si se ingresaron ambos datos
                 if (!usuario.isEmpty() && !contrasena.isEmpty()) {
+                    // Verificar si los datos coinciden con los almacenados en SharedPreferences
                     if (verificarDatosEnSharedPreferences(usuario, contrasena)) {
-                        Intent intent = new Intent(IniciarSesion.this, EditarObjeto.class);
+                        // Datos correctos, iniciar la actividad TipoDeUsuario
+                        Intent intent = new Intent(IniciarSesion.this, TipoDeUsuario.class);
                         startActivity(intent);
                     } else {
+                        // Mostrar mensaje de error si los datos son incorrectos
                         Toast.makeText(IniciarSesion.this, "Usuario o contraseña incorrectos", Toast.LENGTH_SHORT).show();
                     }
                 } else {
+                    // Mostrar mensaje de alerta si no se ingresaron ambos datos
                     Toast.makeText(IniciarSesion.this, "Ingrese usuario y contraseña", Toast.LENGTH_SHORT).show();
                 }
             }
         });
 
+        // Configurar el evento de clic para el botón "CREAR CUENTA"
         btnCrearCuenta.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                // Iniciar la actividad Registrarse1
                 Intent intent = new Intent(IniciarSesion.this, Registrarse1.class);
                 startActivity(intent);
             }
@@ -57,19 +64,8 @@ public class IniciarSesion extends AppCompatActivity {
 
     private boolean verificarDatosEnSharedPreferences(String usuario, String contrasena) {
         SharedPreferences preferences = getSharedPreferences(PREFS_NAME, MODE_PRIVATE);
-
-        // Obtener la lista existente de usuarios
-        Set<String> usuariosGuardados = preferences.getStringSet("usuarios", new HashSet<>());
-
-        // Verificar si el usuario existe
-        if (usuariosGuardados.contains(usuario)) {
-            // Obtener la contraseña asociada con el usuario
-            String storedContrasena = preferences.getString(usuario, "");
-
-            // Verificar si la contraseña coincide
-            return contrasena.equals(storedContrasena);
-        }
-
-        return false; // El usuario no existe
+        String storedUsuario = preferences.getString("usuario", "");
+        String storedContrasena = preferences.getString("contrasena", "");
+        return usuario.equals(storedUsuario) && contrasena.equals(storedContrasena);
     }
 }
--- app/src/main/java/com/cubit/trovami/Registrarse2.java
+++ app/src/main/java/com/cubit/trovami/Registrarse2.java
@@ -10,9 +10,6 @@ import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import java.util.HashSet;
-import java.util.Set;
-
 public class Registrarse2 extends AppCompatActivity {
 
     private static final String PREFS_NAME = "UserData";
@@ -22,30 +19,40 @@ public class Registrarse2 extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registrarse2);
 
+        // Obtener referencias a las vistas
         EditText editTextUsuario = findViewById(R.id.editTextText3);
         EditText editTextContrasena = findViewById(R.id.editTextText4);
         Button btnCrearCuenta = findViewById(R.id.button4);
         Button btnRegresar = findViewById(R.id.button2);
 
+        // Configurar el evento de clic para el botón "CREAR CUENTA"
         btnCrearCuenta.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                // Obtener los datos ingresados
                 String usuario = editTextUsuario.getText().toString().trim();
                 String contrasena = editTextContrasena.getText().toString().trim();
 
+                // Verificar si se ingresaron ambos datos
                 if (!usuario.isEmpty() && !contrasena.isEmpty()) {
+                    // Guardar los datos en SharedPreferences
                     guardarDatosEnSharedPreferences(usuario, contrasena);
+
+                    // Iniciar la actividad IniciarSesion
                     Intent intent = new Intent(Registrarse2.this, IniciarSesion.class);
                     startActivity(intent);
                 } else {
+                    // Mostrar mensaje de alerta si no se ingresaron ambos datos
                     Toast.makeText(Registrarse2.this, "Ingrese usuario y contraseña", Toast.LENGTH_SHORT).show();
                 }
             }
         });
 
+        // Configurar el evento de clic para el botón "REGRESAR"
         btnRegresar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                // Iniciar la actividad Registrarse1
                 Intent intent = new Intent(Registrarse2.this, Registrarse1.class);
                 startActivity(intent);
             }
@@ -53,17 +60,9 @@ public class Registrarse2 extends AppCompatActivity {
     }
 
     private void guardarDatosEnSharedPreferences(String usuario, String contrasena) {
-        SharedPreferences preferences = getSharedPreferences(PREFS_NAME, MODE_PRIVATE);
-        SharedPreferences.Editor editor = preferences.edit();
-
-        // Obtener la lista existente de usuarios
-        Set<String> usuariosGuardados = preferences.getStringSet("usuarios", new HashSet<>());
-
-        // Agregar el nuevo usuario a la lista
-        usuariosGuardados.add(usuario);
-
-        // Guardar la lista actualizada en SharedPreferences
-        editor.putStringSet("usuarios", usuariosGuardados);
+        SharedPreferences.Editor editor = getSharedPreferences(PREFS_NAME, MODE_PRIVATE).edit();
+        editor.putString("usuario", usuario);
+        editor.putString("contrasena", contrasena);
         editor.apply();
     }
 }
