7e9a22e7297a5293a5e15ae9cbecf88db6558aaa
WarReyes <wilfredo.rosales454@gmail.com>
2023-11-11 11:17:38 -0600

    login con mas de un usuario

--- app/src/main/java/com/cubit/trovami/SplashScreen.java
+++ app/src/main/java/com/cubit/trovami/SplashScreen.java
@@ -1,25 +1,45 @@
 package com.cubit.trovami;
+
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.os.Handler;
-
 import androidx.appcompat.app.AppCompatActivity;
 
 public class SplashScreen extends AppCompatActivity {
+
+    private static final String PREFS_NAME = "FirstTimePreferences";
+    private static final String KEY_FIRST_TIME = "isFirstTime";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.splash_screen);
 
-        // Esperar 4 segundos y luego iniciar la actividad Presentacion0
-        new Handler().postDelayed(new Runnable() {
-            @Override
-            public void run() {
-                Intent intent = new Intent(SplashScreen.this, Presentacion0.class);
-                startActivity(intent);
-                finish(); // Cierra la actividad actual para que no pueda regresar a la pantalla de bienvenida.
+        // Verificar si es la primera vez que el usuario ingresa a la aplicación
+        SharedPreferences preferences = getSharedPreferences(PREFS_NAME, MODE_PRIVATE);
+        boolean isFirstTime = preferences.getBoolean(KEY_FIRST_TIME, true);
+
+        // Establecer el tiempo de espera antes de navegar a la siguiente actividad (4 segundos)
+        int splashTimeOut = 2000;
+
+        new Handler().postDelayed(() -> {
+            // Si es la primera vez, navegar a la actividad de presentación
+            if (isFirstTime) {
+                Intent i = new Intent(SplashScreen.this, Presentacion0.class);
+                startActivity(i);
+                finish();
+            } else {
+                // Si no es la primera vez, navegar a la actividad de inicio de sesión
+                Intent i = new Intent(SplashScreen.this, IniciarSesion.class);
+                startActivity(i);
+                finish();
             }
-        }, 4000); // 4000 milisegundos = 4 segundos
+        }, splashTimeOut);
+
+        // Guardar que el usuario ya ha ingresado por primera vez
+        SharedPreferences.Editor editor = preferences.edit();
+        editor.putBoolean(KEY_FIRST_TIME, false);
+        editor.apply();
     }
 }
