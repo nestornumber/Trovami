57f6acf3f703d446f758415110994c9986d74237
nestornumber <nestor.rodriguez999@gmail.com>
2023-11-11 14:19:11 -0600

    boton ajustes y demas cosas

---
+++ app/src/main/java/com/cubit/trovami/MostrarResultados.java
@@ -0,0 +1,14 @@
+package com.cubit.trovami;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MostrarResultados extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_mostrar_resultados);
+    }
+}
