662d45a2399d545a04b527d6e86cfdc77a3ce017
WarReyes <wilfredo.rosales454@gmail.com>
2023-11-15 19:23:24 -0600

    Intentado solucionar problema de galeria

--- app/src/main/java/com/cubit/trovami/EditarObjeto.java
+++ app/src/main/java/com/cubit/trovami/EditarObjeto.java
@@ -1,6 +1,5 @@
 package com.cubit.trovami;
 
-
 import android.Manifest;
 import android.content.Intent;
 import android.content.SharedPreferences;
@@ -56,8 +55,8 @@ public class EditarObjeto extends AppCompatActivity {
         bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
             @Override
             public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-                int id= item.getItemId();
-                if (id == R.id.home){
+                int id = item.getItemId();
+                if (id == R.id.home) {
                     startActivity(new Intent(getApplicationContext(),
                             Busqueda.class));
                     return true;
@@ -67,12 +66,12 @@ public class EditarObjeto extends AppCompatActivity {
                             MostrarResultados.class));
                     return true;
                 }
-                if (id == R.id.edit){
+                if (id == R.id.edit) {
                     startActivity(new Intent(getApplicationContext(),
                             EditarObjeto.class));
                     return true;
                 }
-                if (id == R.id.settings){
+                if (id == R.id.settings) {
                     startActivity(new Intent(getApplicationContext(),
                             Ajustes.class));
                     return true;
@@ -102,7 +101,9 @@ public class EditarObjeto extends AppCompatActivity {
         btnSeleccionarFoto.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                checkGalleryPermission();
+                if (checkGalleryPermission()) {
+                    seleccionarFoto();
+                }
             }
         });
 
@@ -110,15 +111,13 @@ public class EditarObjeto extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 guardarDatosObjeto();
-
             }
         });
+
         if (imagenSeleccionadaUri == null) {
             imageView.setImageResource(R.drawable.editobj_info_image_720p);
         }
-
     }
-
 
     private void tomarFoto() {
         Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
@@ -143,10 +142,9 @@ public class EditarObjeto extends AppCompatActivity {
 
     private boolean checkGalleryPermission() {
         if (ContextCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
-            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.READ_EXTERNAL_STORAGE}, REQUEST_GALLERY_PERMISSION);
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.READ_EXTERNAL_STORAGE, Manifest.permission.MANAGE_EXTERNAL_STORAGE}, REQUEST_GALLERY_PERMISSION);
             return false;
         } else {
-            seleccionarFoto();
             return true;
         }
     }
@@ -181,46 +179,28 @@ public class EditarObjeto extends AppCompatActivity {
                 Bundle extras = data.getExtras();
                 Bitmap imageBitmap = (Bitmap) extras.get("data");
                 imageView.setImageBitmap(imageBitmap);
-
-                // Guarda la imagen en el directorio y obtén la URI
                 Uri imagenUri = guardarImagenEnDirectorioYObtenerUri(editTextNombre.getText().toString().trim(), imageBitmap);
-
-                // Guarda la URI de la imagen
                 imagenSeleccionadaUri = imagenUri;
             } else if (requestCode == REQUEST_IMAGE_PICK && data != null) {
                 imagenSeleccionadaUri = data.getData();
                 imageView.setImageURI(imagenSeleccionadaUri);
-
-                // No necesitas guardar la imagen directamente aquí
             }
         }
     }
 
     private Uri guardarImagenEnDirectorioYObtenerUri(String nombre, Bitmap bitmap) {
         try {
-            // Obtén el directorio externo específico para la aplicación
             File directorioImagenes = new File(getExternalFilesDir(Environment.DIRECTORY_PICTURES), "ObjetosImagenes");
-
-            // Crear el directorio si no existe
             if (!directorioImagenes.exists()) {
                 directorioImagenes.mkdirs();
             }
-
-            // Crear un archivo para la imagen en el directorio
             String nombreArchivo = "imagen_" + nombre + ".jpg";
             File archivoImagen = new File(directorioImagenes, nombreArchivo);
-
-            // Guardar la imagen en el archivo
             try (FileOutputStream out = new FileOutputStream(archivoImagen)) {
                 bitmap.compress(Bitmap.CompressFormat.JPEG, 100, out);
             }
-
-            // Notificar a la galería que se ha agregado una nueva imagen
             MediaStore.Images.Media.insertImage(getContentResolver(), archivoImagen.getAbsolutePath(), archivoImagen.getName(), archivoImagen.getName());
-
-            // Devuelve la URI de la imagen guardada
             return Uri.fromFile(archivoImagen);
-
         } catch (IOException e) {
             e.printStackTrace();
             return null;
@@ -252,14 +232,11 @@ public class EditarObjeto extends AppCompatActivity {
         }
 
         if (imagenSeleccionadaUri != null) {
-            // Guardar la URI de la imagen
             editor.putString(nombre + "_imagen", imagenSeleccionadaUri.toString());
         }
 
         editor.apply();
-
         limpiarCampos();
-
         Toast.makeText(this, "Objeto agregado", Toast.LENGTH_SHORT).show();
     }
 
